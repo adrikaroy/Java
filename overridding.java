39c5eb98d19bbda43af5bb146016ee40fe5f2ac1
adrikaroy <adrika.roy17@gmail.com>
2019-08-06 14:13:11 +0530

    Overridding done

--- overridding.java
+++ overridding.java
@@ -19,15 +19,60 @@ class Car{
 
 	   }
 	   int getSpeed(){
-	   	return(50)
+	   	return(50);
 	   }
 	   int getWheel(){
-	   	return(4)
+	   	return(4);
 	   }
-	   getSeat()
+	   int getSeat(){
+	   	return(6);
+	   }
 	}
-	SportsCar:
-	   getColor()
-	   getSpeed()
-	   getWheel()
-	   getSeat() 
+
+class SportsCar extends Car{
+	   String getColor(){
+	   	return("white");
+
+	   }
+	   int getSpeed(){
+	   	return(120);
+	   }
+	   int getWheel(){
+	   	return(5);
+	   }
+	   int getSeat(){
+	   	return(2);
+	   }
+	}
+
+class Main{
+	public static void main(String[] args) {
+		SportsCar c = new SportsCar();
+		System.out.println(c.getColor());
+		System.out.println(c.getSpeed());
+		System.out.println(c.getWheel());
+		System.out.println(c.getSeat());
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
